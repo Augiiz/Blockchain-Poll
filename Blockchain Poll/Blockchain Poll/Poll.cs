42744cda80897d16e535ac0c24e57200cef3f507
Augiz <augis916@gmail.com>
2019-05-18 16:10:34 +0300

    C# App

--- Blockchain Poll/Blockchain Poll/Poll.cs
+++ Blockchain Poll/Blockchain Poll/Poll.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Blockchain_Poll
 {
-    public partial class Form1 : Form
+    public partial class Poll : Form
     {
-        public Form1()
+        public Poll()
         {
             InitializeComponent();
         }
